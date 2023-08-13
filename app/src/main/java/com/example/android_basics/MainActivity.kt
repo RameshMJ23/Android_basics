2cd5bda551895fd7d5e0ae53152a0d196ab157dd
ramesh <rameshkrish022000@gmail.com>
2023-08-13 07:31:22 +0300

    radio button and checkbox

--- app/src/main/java/com/example/android_basics/MainActivity.kt
+++ app/src/main/java/com/example/android_basics/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.android_basics
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_checkbox_radiobutton.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_constraint)
+        setContentView(R.layout.activity_checkbox_radiobutton)
+
+        btnOrder.setOnClickListener{
+
+            val meatSelected = findViewById<RadioButton>(rgMeat.checkedRadioButtonId)
+                .text.toString()
+
+            val finalOrder = "You ordered: \n" +
+                    "$meatSelected \n" +
+                    (if(cbCheese.isChecked) "Su cheese\n" else "") +
+                    (if(cbOnion.isChecked) "Su Onion\n" else "") +
+                    (if(cbSalad.isChecked) "Su Salad\n" else "")
+
+            tvFinalOrder.text = finalOrder
+        }
     }
 }
