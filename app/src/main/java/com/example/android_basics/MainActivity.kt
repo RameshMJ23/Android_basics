18e32a0f035010a54005c4fbce7e634875aec287
ramesh <rameshkrish022000@gmail.com>
2023-08-19 22:46:30 +0300

    Intents and passing msgs

--- app/src/main/java/com/example/android_basics/MainActivity.kt
+++ app/src/main/java/com/example/android_basics/MainActivity.kt
@@ -1,28 +1,40 @@
 package com.example.android_basics
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_toast.*
-import kotlinx.android.synthetic.main.custom_toast.*
+import kotlinx.android.synthetic.main.activity_main_first_screen.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_toast)
+        setContentView(R.layout.activity_main_first_screen)
 
-        //Simple toast
-        /*btnToast.setOnClickListener{
-            Toast.makeText(this, "Simple toast!", Toast.LENGTH_SHORT).show()
-        }*/
+        firstScBtn.setOnClickListener{
 
-        btnToast.setOnClickListener{
-            Toast(applicationContext).apply {
-                duration = Toast.LENGTH_SHORT
-                view = layoutInflater.inflate(R.layout.custom_toast, clToast)
+            val name = etName.text.toString()
 
-                show()
+            val age = etAge.text.toString().toInt()
+
+            val country = etCountry.text.toString()
+
+            val personDetails = Person(name, age, country)
+
+            Intent(this, SecondActivity::class.java).also {
+
+                //With individual values
+                /*it.putExtra("EXTRA_NAME", name)
+
+                it.putExtra("EXTRA_AGE", age)
+
+                it.putExtra("EXTRA_COUNTRY", country)*/
+
+                //With data classes
+                it.putExtra("EXTRA_DETAILS", personDetails)
+
+                startActivity(it)
             }
+
         }
     }
 }
--- app/src/main/java/com/example/android_basics/second_activity.kt
+++ app/src/main/java/com/example/android_basics/second_activity.kt
@@ -2,10 +2,28 @@ package com.example.android_basics
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_second.*
 
-class second_activity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        //With individual values
+        /*val personName = intent.getStringExtra("EXTRA_NAME")
+
+        val personAge = intent.getIntExtra("EXTRA_AGE", 0)
+
+        val personCountry = intent.getStringExtra("EXTRA_COUNTRY")*/
+
+        val personDetails = intent.getSerializableExtra("EXTRA_DETAILS") as Person
+
+        saTextView.text = personDetails.toString()
+
+        saBtnBack.setOnClickListener{
+            finish()
+        }
+
     }
 }
