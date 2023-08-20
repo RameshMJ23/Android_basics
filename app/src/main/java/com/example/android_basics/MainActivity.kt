f26a9d270529572f4d1acb3df23e9124241f3e91
ramesh <rameshkrish022000@gmail.com>
2023-08-20 21:08:55 +0300

    Alert dialogs

--- app/src/main/java/com/example/android_basics/MainActivity.kt
+++ app/src/main/java/com/example/android_basics/MainActivity.kt
@@ -1,34 +1,72 @@
 package com.example.android_basics
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_alert_dialogues.*
 
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alert_dialogues)
 
-    }
+        val options = arrayOf("Morn", "Noon", "Night")
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.app_bar_menu, menu)
-        return true;
-    }
+        val normalDialog = AlertDialog.Builder(this)
+            .setTitle("Add Alarm")
+            .setIcon(R.drawable.ic_alarm)
+            .setMessage("Do you want to set Alarm")
+            .setPositiveButton("Yes"){dialogInterface, i ->
+                Toast.makeText(this, "Alarm created", Toast.LENGTH_SHORT).show()
+            }
+            .setNegativeButton("no"){dialogInterface, i ->
+                Toast.makeText(this, "Alarm not created", Toast.LENGTH_SHORT).show()
+            }.create()
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val radioDialog = AlertDialog.Builder(this)
+            .setTitle("Select period")
+            .setIcon(R.drawable.ic_alarm)
+            .setSingleChoiceItems(options, 0){dialogInterface, i ->
+                Toast.makeText(this, "${options[i]} is selected", Toast.LENGTH_SHORT).show()
+            }
+            .setPositiveButton("Yes"){dialogInterface, i ->
+                Toast.makeText(this, "Alarm created", Toast.LENGTH_SHORT).show()
+            }
+            .setNegativeButton("no"){dialogInterface, i ->
+                Toast.makeText(this, "Alarm not created", Toast.LENGTH_SHORT).show()
+            }.create()
 
-        when(item.itemId){
-            R.id.miAlarm -> Toast.makeText(this, "Alarm clicked", Toast.LENGTH_SHORT).show()
-            R.id.miAddContact -> Toast.makeText(this, "Add contact clicked", Toast.LENGTH_SHORT).show()
-            R.id.miTakePhoto -> Toast.makeText(this, "Took photo", Toast.LENGTH_SHORT).show()
-            R.id.miClose -> finish()
+        val multipleOptionDialog = AlertDialog.Builder(this)
+            .setTitle("Select periods")
+            .setIcon(R.drawable.ic_alarm)
+            .setMultiChoiceItems(options, booleanArrayOf(true, true, false)){ dialogInterface, i, isChecked->
+                if(isChecked){
+                    Toast.makeText(this, "${options[i]} is checked", Toast.LENGTH_SHORT).show()
+                }else{
+                    Toast.makeText(this, "${options[i]} is unchecked", Toast.LENGTH_SHORT).show()
+                }
+            }
+            .setPositiveButton("Yes"){dialogInterface, i ->
+                Toast.makeText(this, "Alarm created", Toast.LENGTH_SHORT).show()
+            }
+            .setNegativeButton("no"){dialogInterface, i ->
+                Toast.makeText(this, "Alarm not created", Toast.LENGTH_SHORT).show()
+            }.create()
+
+        btnNormalDialog.setOnClickListener{
+            normalDialog.show()
         }
-        return true
-    }
 
+        btnCheckDialog.setOnClickListener{
+            multipleOptionDialog.show()
+        }
+
+        btnRadioDialog.setOnClickListener{
+            radioDialog.show()
+        }
+    }
 
 }
