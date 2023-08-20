7d357f1ab3bdd15862a49c713c0b4ba36bb25621
ramesh <rameshkrish022000@gmail.com>
2023-08-20 20:37:15 +0300

    custom menu bar

--- app/src/main/java/com/example/android_basics/MainActivity.kt
+++ app/src/main/java/com/example/android_basics/MainActivity.kt
@@ -1,86 +1,34 @@
 package com.example.android_basics
 
-import android.Manifest
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Log
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import kotlinx.android.synthetic.main.activity_main_permissions.*
+
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var permissionLauncher: ActivityResultLauncher<String>
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_permissions)
-
-
-        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){
-            permissions -> Log.d("MainActivityPermissions", permissions.toString())
-        }
-
-        btnPermissions.setOnClickListener{
-            requestPermissions()
-        }
-
-        ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION)
 
     }
 
-    private fun checkFgLocationPermission() = ContextCompat.checkSelfPermission(
-        applicationContext,
-        Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
-
-    private fun checkStoragePermission() = ContextCompat.checkSelfPermission(
-        applicationContext,
-        Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
-
-    private fun checkBgLocationPermission() = ContextCompat.checkSelfPermission(
-        applicationContext,
-        Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED
-
-
-    private fun requestPermissions(){
-
-        val permissionArray = mutableListOf<String>()
-
-        /*if(!checkFgLocationPermission()){
-           permissionArray.add(Manifest.permission.ACCESS_COARSE_LOCATION)
-        }
-        if(!checkBgLocationPermission()){
-           permissionArray.add(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
-        }*/
-
-        if(!checkStoragePermission()){
-           permissionArray.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-        }
-
-        if(permissionArray.isNotEmpty()){
-            Log.d("MainActivityPermissions", "Permission array is not empty")
-            //ActivityCompat.requestPermissions(this,permissionArray.toTypedArray(), 0)
-
-            permissionLauncher.launch(permissionArray[0])
-        }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.app_bar_menu, menu)
+        return true;
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
-        for(i in grantResults.indices){
-
-            if(grantResults[i] == PackageManager.PERMISSION_GRANTED){
-                Log.d("MainActivityPermissions", "Permission granted for ${permissions[i]}, with $requestCode")
-            }
+        when(item.itemId){
+            R.id.miAlarm -> Toast.makeText(this, "Alarm clicked", Toast.LENGTH_SHORT).show()
+            R.id.miAddContact -> Toast.makeText(this, "Add contact clicked", Toast.LENGTH_SHORT).show()
+            R.id.miTakePhoto -> Toast.makeText(this, "Took photo", Toast.LENGTH_SHORT).show()
+            R.id.miClose -> finish()
         }
+        return true
     }
+
 
 }
